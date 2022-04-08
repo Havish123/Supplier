c962055d7bd13eed5ca85a3e4cc64cf7c83c03e3
Havish123 <havishnadar@gmail.com>
2022-04-08 11:48:02 +0530

    Modified

--- SupplierAPI/Data/DBInitializer.cs
+++ SupplierAPI/Data/DBInitializer.cs
@@ -6,75 +6,78 @@ namespace SupplierAPI.Data
     {
         public static void Initializer(SupplierDBContext context)
         {
-            if (context.Brands.Any() || context.Categories.Any())
+            if (!context.Categories.Any())
             {
-                return;
+                List<Category> category = new List<Category>()
+                {
+                    new Category{CategoryName="Beverages"},
+                    new Category{CategoryName="Condiments"},
+                    new Category{CategoryName="Congections"},
+                    new Category{CategoryName="Dairy Products"},
+                    new Category{CategoryName="Grains/Cereals"},
+                    new Category{CategoryName="Meat/Poultry"},
+                    new Category{CategoryName="Produce"},
+                    new Category{CategoryName="Seafood"},
+                };
+                context.Categories.AddRange(category);
+                context.SaveChanges();
             }
-            List<Category> category = new List<Category>()
+            if (!context.Brands.Any())
             {
-                new Category{CategoryName="Beverages"},
-                new Category{CategoryName="Condiments"},
-                new Category{CategoryName="Congections"},
-                new Category{CategoryName="Dairy Products"},
-                new Category{CategoryName="Grains/Cereals"},
-                new Category{CategoryName="Meat/Poultry"},
-                new Category{CategoryName="Produce"},
-                new Category{CategoryName="Seafood"},
-            };
-            context.Categories.AddRange(category);
-            context.SaveChanges();
+                List<Brand> brands = new List<Brand>() {
+                    new Brand{BrandName="Nestle"},
+                    new Brand{BrandName="SunFeast"},
+                    new Brand{BrandName="Cadbery"},
+                    new Brand{BrandName="HUL"},
+                };
 
-            List<Brand> brands = new List<Brand>() { 
-                new Brand{BrandName="Nestle"},
-                new Brand{BrandName="SunFeast"},
-                new Brand{BrandName="Cadbery"},
-                new Brand{BrandName="HUL"},
-            };
+                context.Brands.AddRange(brands);
+                context.SaveChanges();
+            }
+            if (!context.Suppliers.Any()) {
+                List<Supplier> suppliers = new List<Supplier>() {
+                    new Supplier{
+                        supplierName="Deepak",
+                        supplierEmail="deepakraj@gmail.com",
+                        supplierPhone="1234567890",
+                        supplierAddress="Madurai",
+                        supplierBulstat="118585306",
 
-            context.Brands.AddRange(brands);
-            context.SaveChanges();
+                    },new Supplier{
+                        supplierName="Dharani",
+                        supplierEmail="dharani@gmail.com",
+                        supplierPhone="1234567890",
+                        supplierAddress="Coimbatore",
+                        supplierBulstat="118585305",
+
+                    },new Supplier{
+                        supplierName="Vinil",
+                        supplierEmail="vinil@gmail.com",
+                        supplierPhone="1234567890",
+                        supplierAddress="Tirunelveli",
+                        supplierBulstat="118585303",
+
+                    },new Supplier{
+                        supplierName="Sudharshan",
+                        supplierEmail="sudharshan@gmail.com",
+                        supplierPhone="1234567890",
+                        supplierAddress="Theni",
+                        supplierBulstat="118585302",
+
+                    },new Supplier{
+                        supplierName="Suwathi",
+                        supplierEmail="suwathi@gmail.com",
+                        supplierPhone="1234567890",
+                        supplierAddress="Tirupur",
+                        supplierBulstat="118585301",
+
+                    },
+                };
+                context.Suppliers.AddRange(suppliers);
+                context.SaveChanges();
+            }
 
 
-            List<Supplier> suppliers = new List<Supplier>() {
-                new Supplier{
-                    supplierName="Deepak",
-                    supplierEmail="deepakraj@gmail.com",
-                    supplierPhone="1234567890",
-                    supplierAddress="Madurai",
-                    supplierBulstat="118585306",
-
-                },new Supplier{
-                    supplierName="Dharani",
-                    supplierEmail="dharani@gmail.com",
-                    supplierPhone="1234567890",
-                    supplierAddress="Coimbatore",
-                    supplierBulstat="118585305",
-
-                },new Supplier{
-                    supplierName="Vinil",
-                    supplierEmail="vinil@gmail.com",
-                    supplierPhone="1234567890",
-                    supplierAddress="Tirunelveli",
-                    supplierBulstat="118585303",
-
-                },new Supplier{
-                    supplierName="Sudharshan",
-                    supplierEmail="sudharshan@gmail.com",
-                    supplierPhone="1234567890",
-                    supplierAddress="Theni",
-                    supplierBulstat="118585302",
-
-                },new Supplier{
-                    supplierName="Suwathi",
-                    supplierEmail="suwathi@gmail.com",
-                    supplierPhone="1234567890",
-                    supplierAddress="Tirupur",
-                    supplierBulstat="118585301",
-
-                },
-            };
-            context.Suppliers.AddRange(suppliers);
-            context.SaveChanges();
         }
 
     }
