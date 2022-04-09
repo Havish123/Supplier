8d2efaef5b2b6168a9e371b4c41421cc9c8d7562
Havish123 <havishnadar@gmail.com>
2022-04-09 22:13:51 +0530

    Add Style to the buttons

--- SupplierMVC/Models/ProductCreateModel.cs
+++ SupplierMVC/Models/ProductCreateModel.cs
@@ -1,12 +1,16 @@
 ﻿using SupplierMVC.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace SupplierMVC.Models
 {
     public class ProductCreateModel
     {
         public ProductData productData { get; set; }
+        [Display(Name ="Categories")]
         public IEnumerable<CategoryData> categories { get; set; }
+        [Display(Name ="Suppliers")]
         public IEnumerable<SupplierData> suppliers { get; set; }
+        [Display(Name ="Brands")]
         public IEnumerable<BrandData> brands { get; set; }
     }
 }
